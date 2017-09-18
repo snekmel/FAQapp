add1942cc627fc2a528a4fb9ca37648f59bce70d
Lars <larslemkens@gmail.com>
2017-09-18 20:20:56 +0200

    Hoofdstuk 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuestionListComponent } from './components/question-list/question-list.component';
-import {DataService} from './services/data.service'
+import {DataService} from './services/data.service';
+import { QuestionComponent } from './components/question/question.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    QuestionListComponent
+    QuestionListComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
