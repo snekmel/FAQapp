f7e9ae4a375c6d4273ab2ab3de25fed77c40a0cc
Lars <larslemkens@gmail.com>
2017-09-24 13:11:12 +0200

    Klaar

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Question} from '../../models/Question';
+import {DataService} from '../../services/data.service';
 
 @Component({
   selector: 'app-question',
@@ -9,9 +10,13 @@ import {Question} from '../../models/Question';
 export class QuestionComponent implements OnInit {
 @Input('question') question:Question;
 
-  constructor() { }
+  constructor(public dataService:DataService) { }
 
   ngOnInit() {
+  }
+
+  removeQuestion(question){
+    this.dataService.removeQuestion(question);
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,32 +6,68 @@ export class DataService {
 questions:Question[];
 
   constructor() { 
-
+/*
     this.questions = [{
-      text:"What is your name?",
+      text:'What is your name?',
       answer:"My name is Lars",
       hide:true
     },
     {
-      text:"What is your favourite color?",
+      text:'What is your favourite color?',
       answer:"Green",
       hide:true
     },
     {
-      text:"What is your favorite languague?",
+      text:'What is your favorite languague?',
       answer:"Javascript",
       hide:true
     }
   ];
+
+  */
   }
 
 
   getQuestions(){
+    if(localStorage.getItem('questions')=== null){
+      this.questions = [];
+    }else{
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
     return this.questions;
   }
 
   addQuestion(question:Question){
     this.questions.unshift(question);
+    //Local var
+    let questions;
+
+    if(localStorage.getItem('questions')=== null){
+     questions = [];
+     questions.unshift(question);
+     localStorage.setItem('questions', JSON.stringify(questions));
+
+    }else{
+questions = JSON.parse(localStorage.getItem('questions'));
+questions.unshift(question);
+this.questions = JSON.parse(localStorage.getItem('questions'));
+   
+
+localStorage.setItem('questions',JSON.stringify(questions));
+}
   }
 
+  removeQuestion(question:Question){
+
+    console.log(question);
+    for (var index = 0; index < this.questions.length; index++) {
+    
+if(question == this.questions[index]){
+this.questions.splice(index, 1);
+localStorage.setItem('questions', JSON.stringify(this.questions));
+
+}
+
+    }
+  }
 }
